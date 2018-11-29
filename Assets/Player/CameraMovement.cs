2ba6fd0a30e5b7631f21afda6e9208534bad407c
Federico "Magus" Tomas Rocca <frocca17@gmail.com>
2018-11-29 00:21:21 -0300

    Primera versión de movimiento de personaje Agregado de camara que sigue al personaje

---
+++ Assets/Player/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform PlayerTransform;
+    public Vector3 CameraBuffer;
+    private Transform CameraTransform;
+
+    void Update()
+    {
+        if (PlayerTransform != null)
+        {
+            CameraTransform.position = PlayerTransform.position + CameraBuffer;
+        }
+    }
+
+    private void Start()
+    {
+        CameraTransform = GetComponent<Transform>();
+    }
+}
