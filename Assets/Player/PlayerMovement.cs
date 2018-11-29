38acb1621dba0c1e15f9f1dba123e358ce0f262c
Federico "Magus" Tomas Rocca <frocca17@gmail.com>
2018-11-29 01:59:26 -0300

    Se agrega salto y rotación al cambiar de dirección al personaje

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -5,8 +5,11 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed;
+    public float Rotationspeed;
+    public float JumpForce;
 
     private Rigidbody rb;
+    private bool isGrounded = true;
 
     void Start()
     {
@@ -18,8 +21,34 @@ public class PlayerMovement : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        // Rotación
+        if( moveHorizontal > 0 )
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, 0), Rotationspeed * Time.deltaTime);
+        }
+        if( moveHorizontal < 0 )
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, -180, 0), Rotationspeed * Time.deltaTime);
+        }
 
+        // Calculo del movimiento
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        Debug.Log("moveVertical " + moveVertical);
+        // Salto
+        if ((moveVertical > 0 || Input.GetKeyDown(KeyCode.Space)) && isGrounded == true)
+        {
+            movement.y = 1 * JumpForce;
+            isGrounded = false;
+        }
+
+        // Movimiento
         rb.AddForce(movement * speed);
+    }
+
+    // Reseteo del salto
+    void OnCollisionStay()
+    {
+        isGrounded = true;
     }
 }
